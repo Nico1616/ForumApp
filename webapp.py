8c0a802ea0c97938993000907843393ede967930
Nico1616 <54326646+Nico1616@users.noreply.github.com>
2020-05-27 14:28:06 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,29 +1,25 @@
 from flask import Flask, redirect, url_for, session, request, jsonify, Markup
 from flask_oauthlib.client import OAuth
 from flask import render_template
-from profanityfilter import ProfanityFilter
+
 
 import pprint
 import os
 import pymongo
 
 
-# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
-# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
-# environment variables, so that this will work on Heroku.
-# Edited by S. Adams for Designing Software for the Web to add comments and remove flash messaging
 
 app = Flask(__name__)
 
-app.debug = True #Change this to False for production
+app.debug = True 
 
 connection_string = os.environ["MONGO_CONNECTION_STRING"]
 db_name = os.environ["MONGO_DBNAME"]
     
 client = pymongo.MongoClient(connection_string)
-db = client[db_name]
+db = client['test']
 
-collection = db['Forum-PostsCol'] 
+collection = db['people'] 
 
 app.secret_key = os.environ['SECRET_KEY'] 
 oauth = OAuth(app)
@@ -52,43 +48,8 @@ user_not_valid = []
 
 @app.route('/')
 def home():
-    super_secret_data = ''
-    super_secret_data2 = ''
-    global user_valid
-    global user_not_valid
     
-    post_info = ''
-    for a in collection.find():
-        print('hi')
-        post_info += Markup('<div class=\"eachPost\">'+'<h3>' + a['fname'] + ' ' + a['lname'] + '</h3><br>' + '<p class=\"paragraphPost\">' + a['message'] + '</p>' + '</div>')
-    
-    if 'user_data' in session and session['user_data']['public_repos'] > 10:
-        user_check = True#pprint.pformat(session['user_data'])#format the user data nicely
-        user_valid.append(session['user_data']['login'])
-        if session['user_data']['login'] == 'LucaCC':
-            for x in user_valid:
-                try:
-                    super_secret_data.index(x) #checking for error
-                except Exception as inst:
-                    super_secret_data += x + ' '
-            for y in user_not_valid:
-                try:
-                    super_secret_data2.index(y)
-                except Exception as inst:
-                    super_secret_data2 += y + ' '
-            admin_check = 'Admin Privileges'
-        else:
-            super_secret_data = ''
-            super_secret_data2 = ''
-            admin_check = ''
-    else:
-        user_check = False
-        super_secret_data = ''
-        super_secret_data2 = ''
-        admin_check = ''
-        if 'user_data' in session:
-            user_not_valid.append(session['user_data']['login'])
-    return render_template('home.html',post=post_info, valid_user=user_check, admin_secret_data=super_secret_data, admin_secret_data2=super_secret_data2, Admin=admin_check)
+    return render_template('home.html')
 
 @app.route('/login')
 def login():   
@@ -98,11 +59,11 @@ def login():
 def logout():
     session.clear()
     return render_template('message.html', message='You were logged out')
+    
 @app.route('/postcheck', methods=['POST'])
 def post_check():
-    pf = ProfanityFilter()
-    if 'user_data' in session and len(request.form['message'].split(' '))/session['user_data']['public_repos'] <= 5:
-        doc = {'fname': request.form['fname'],'lname': request.form['lname'],'message': pf.censor(request.form['message'])}
+    if 'user_data' in session and len(request.form['message'].split(' '))/session['user_data']['public_repos'] <= 10:
+        doc = {'fname': request.form['fname'],'lname': request.form['lname'],'message': request.form['message']}
         collection.insert_one(doc)
         return render_template('message.html', message='You successfully posted')
     return render_template('message.html', message='You failed to post due to too many words for your repository count')
